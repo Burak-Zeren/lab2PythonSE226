03e436bc5daded35afd8cbb2035428bbca2efd22
Muhammed Burak Zeren <opkbmz@outlook.com>
2023-03-14 11:28:40 +0300

    error

--- lab2_2.py
+++ lab2_2.py
@@ -1,9 +1,18 @@
 number=int(input("Please enter between 3 and 9 (inclusive) : "))
+
 for i in range(1, number+1):
+    if number < 3 | number > 9:
+        break
     if i == number:
         print('*' * i + f" {number} stars")
         break
     print('*' * i)
 
 for j in range(number - 1, 0,-1):
-            print('*' * j)
+    if number < 3 | number > 9:
+        break
+    print('*' * j)
+
+if number < 3 | number > 9:
+     print("You entered wrong number !")
+
